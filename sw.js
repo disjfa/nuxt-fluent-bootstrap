8f87b8bd323754f325de3299d679ce48163d06fb
disjfa <disjfa@disjfa.nl>
2017-10-19 21:29:27 +0000

    Updates

--- sw.js
+++ sw.js
@@ -19,31 +19,31 @@ importScripts('workbox-sw.prod.v1.3.0.js');
  */
 const fileManifest = [
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrapapp.4912cf688fb10a8ba2fa.js",
+    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/app.4912cf688fb10a8ba2fa.js",
     "revision": "e1534b35f3d5e9bfd0bfaa14730859b4"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrapcommon.6bc6ca04799cd9cb5af8.js",
+    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/common.6bc6ca04799cd9cb5af8.js",
     "revision": "36b772c9c7051b28a4e27259b338b1b7"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrapcommon.ac9345fdf57117098439cc74f77015b2.css",
-    "revision": "420a30fba650801127b3b208bb53e6f0"
+    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/common.c8668266d4c86592058b3a9f7219cd59.css",
+    "revision": "07e3d9098838713937730bb0202ae552"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstraplayouts/default.55344220c5814ec85504.js",
+    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/layouts/default.55344220c5814ec85504.js",
     "revision": "429dc1e726229409b7ffef57ab29040c"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrapmanifest.51ccd5946956e69a18c2.js",
-    "revision": "9a85675e4c93c00460954e3df7655da2"
+    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/manifest.5e5d5c70b0694aa8be56.js",
+    "revision": "ef07865a91a31bd2f8aef9971bd7bbb1"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrappages/form.ea57ac993c0bb2d20605.js",
+    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/pages/form.ea57ac993c0bb2d20605.js",
     "revision": "0229df82fe680c32c8bedcc3190367cb"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrappages/index.3e09192243a35aa7c83f.js",
+    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/pages/index.3e09192243a35aa7c83f.js",
     "revision": "63ba76a94be89b96ec1179d6d199efa9"
   }
 ];
