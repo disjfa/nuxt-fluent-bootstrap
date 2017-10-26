abc3ed8cf4e26ccb8fd9db226fa8ac9f0a90e415
disjfa <disjfa@disjfa.nl>
2017-10-26 20:49:12 +0000

    Updates

--- sw.js
+++ sw.js
@@ -19,32 +19,32 @@ importScripts('workbox-sw.prod.v1.3.0.js');
  */
 const fileManifest = [
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/app.4912cf688fb10a8ba2fa.js",
-    "revision": "e1534b35f3d5e9bfd0bfaa14730859b4"
+    "url": "/_nuxt/app.b775fdbacf77cc8a1fe4.js",
+    "revision": "0b096e826645a1ad411c0cd7e0be7015"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/common.6bc6ca04799cd9cb5af8.js",
+    "url": "/_nuxt/common.68902de77d0816a451efe47a6e85781d.css",
+    "revision": "d9295ff133a9eed1587b06fe8497367d"
+  },
+  {
+    "url": "/_nuxt/common.6bc6ca04799cd9cb5af8.js",
     "revision": "36b772c9c7051b28a4e27259b338b1b7"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/common.c8668266d4c86592058b3a9f7219cd59.css",
-    "revision": "07e3d9098838713937730bb0202ae552"
+    "url": "/_nuxt/layouts/default.324a25d49ad7575d3328.js",
+    "revision": "adff15508a35dd845c12299960ebad78"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/layouts/default.55344220c5814ec85504.js",
-    "revision": "429dc1e726229409b7ffef57ab29040c"
+    "url": "/_nuxt/manifest.7df2d2e778ade1b0387b.js",
+    "revision": "e537173ea28274fe94229568ea7cf0c2"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/manifest.5e5d5c70b0694aa8be56.js",
-    "revision": "ef07865a91a31bd2f8aef9971bd7bbb1"
+    "url": "/_nuxt/pages/form.07e30a033f419ef53185.js",
+    "revision": "d21ece27cd88a5efe3eb0efa7e97e2c3"
   },
   {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/pages/form.ea57ac993c0bb2d20605.js",
-    "revision": "0229df82fe680c32c8bedcc3190367cb"
-  },
-  {
-    "url": "/nuxt-fluent-bootstrap/fluent-bootstrap/pages/index.3e09192243a35aa7c83f.js",
-    "revision": "63ba76a94be89b96ec1179d6d199efa9"
+    "url": "/_nuxt/pages/index.75808d042ef2d5a49c71.js",
+    "revision": "8e4ad14fd4482c47554831a589f076d8"
   }
 ];
 
@@ -54,5 +54,5 @@ const workboxSW = new self.WorkboxSW({
   "directoryIndex": "/"
 });
 workboxSW.precache(fileManifest);
-workboxSW.router.registerRoute('/nuxt-fluent-bootstrap/**', workboxSW.strategies.networkFirst({}), 'GET');
-workboxSW.router.registerRoute('/nuxt-fluent-bootstrap/fluent-bootstrap/**', workboxSW.strategies.cacheFirst({}), 'GET');
+workboxSW.router.registerRoute('/**', workboxSW.strategies.networkFirst({}), 'GET');
+workboxSW.router.registerRoute('/_nuxt/**', workboxSW.strategies.cacheFirst({}), 'GET');
